301a2800850f47d5a19b6028436168d4fd44409b
Aleksei da Silva <alekseidasilva@hotmail.com>
2020-04-16 08:09:19 +0100

    Perfil_Usuario

---
+++ SiGMun/SiGMun.DB/Tabelas/TB_Usuario_Perfil.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TB_Usuario_Perfil]
+(
+	UsuPerilId INT NOT NULL,
+	UsuId int not null,
+	UsuIdPerfil int not null
+
+	CONSTRAINT PK_Usuario_Perfil_Id PRIMARY KEY (UsuPerilId),
+    CONSTRAINT FK_UsuarioId FOREIGN KEY (UsuId) REFERENCES TB_Usuarios,
+    CONSTRAINT FK_UsuIDdoPerfil FOREIGN KEY (UsuIdPerfil) REFERENCES TB_Perfil_Usuario
+
+)
